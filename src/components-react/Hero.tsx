7bb0028d2481e98a09929731df0497453ab1f7e5
adris34 <adrian.stepien34470@gmail.com>
2026-07-08 19:56:17 +0200

    Restore video in Hero

--- src/components-react/Hero.tsx
+++ src/components-react/Hero.tsx
@@ -78,6 +78,18 @@ export const Hero = () => {
               ))}
             </div>
 
+            <div className="w-full max-w-5xl mx-auto rounded-2xl overflow-hidden shadow-2xl">
+              <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
+                <iframe
+                  src="https://www.youtube.com/embed/zBVOvBmkQMM"
+                  title="Adonyme"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="absolute inset-0 w-full h-full"
+                />
+              </div>
+            </div>
+
             <Button
               asChild
               size="lg"
